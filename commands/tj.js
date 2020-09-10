4392c3f6abdc6344259fe6a1c7443e1fce37a818
Alexei Trofimov <alexei24@list.ru>
2020-09-10 11:33:54 +0300

    Fixing TJ ending date and getting rid of ugly af else if hell

--- commands/tj.js
+++ commands/tj.js
@@ -1,12 +1,32 @@
 const { telegram } = require('../index')
 
+const untilEndMsg = [
+    {
+        300: 'KOMPPANIASSA HERÄTYS!',
+        281: 'Menisitte tekin oikeisiin töihin',
+        270: 'https://www.youtube.com/watch?v=26zVagt8bck',
+        200: 'Tuntuuko kaksataa aamua oikeesti enään missään?',
+        182: 'Itsehän en pidä puolenvuoden miehiä juuri minään.',
+        173: 'Hei puolet käyty jo!',
+        112: 'https://www.youtube.com/watch?v=iN43sSF-Q3k',
+        108: 'https://www.youtube.com/watch?v=EnbjR635kfc',
+        100: 'Sata salamaa iskee tulta!',
+        99: 'https://www.youtube.com/watch?v=6uikJTnmtgw',
+        92: 'Ei vielä saa luovuttaa.',
+        90: 'https://www.youtube.com/watch?v=kxLwGow0Tvw',
+        69: 'Heh https://www.youtube.com/watch?v=9f06QZCVUHg',
+        10: "It's the final countdown!",
+        0: 'Ohi on!! https://www.youtube.com/watch?v=uWF4DXP8ZFc'
+    }
+]
+
 exports.tj = {
   help: 'Kertoo nykyisen tj:n',
   usage: '/tänäänjäljellä',
   aliases: ['tjlaskuri'],
   func: (args, update) => {
     const startDate = new Date(2020, 6, 6)
-    const endDate = new Date(2021, 5, 18)
+    const endDate = new Date(2021, 5, 17)
     const today = new Date()
 
     const dayDiff = (d1, d2) => {
@@ -36,38 +56,15 @@ exports.tj = {
             { parse_mode: "Markdown", disable_notification: true })
     }
     else if (today > startDate) {
+        
         let msg = ''
-        if (untilEnd === 346) {
-            msg = 'KOMPPANIASSA HERÄTYS!'
-        } else if (untilEnd === 300) {
-            msg = 'Oi oi sataset paukkuu jo!'
-        } else if (untilEnd === 270) {
-            msg = 'https://www.youtube.com/watch?v=26zVagt8bck'
-        } else if (untilEnd === 200) {
-            msg = 'Tuntuuko kaksataa aamua oikeesti enään missään?'
-        } else if (untilEnd === 182) {
-            msg = 'Itsehän en pidä puolenvuoden miehiä juuri minään.'
-        } else if (untilEnd === 173) {
-            msg = 'Hei puolet käyty jo!'
-        } else if (untilEnd === 112) {
-            msg = 'https://www.youtube.com/watch?v=iN43sSF-Q3k'
-        } else if (untilEnd === 108) {
-            msg = 'https://www.youtube.com/watch?v=EnbjR635kfc'
-        } else if (untilEnd === 100) {
-            msg = 'Sata salamaa iskee tulta!'
-        } else if (untilEnd === 99) {
-            msg = 'https://www.youtube.com/watch?v=6uikJTnmtgw'
-        } else if (untilEnd === 92) {
-            msg = 'Ei vielä saa luovuttaa.'
-        } else if (untilEnd === 90) {
-            msg = 'https://www.youtube.com/watch?v=kxLwGow0Tvw'
-        } else if (untilEnd === 69) {
-            msg = 'Heh https://www.youtube.com/watch?v=9f06QZCVUHg'
-        } else if (untilEnd === 10) {
-            msg = "It's the final countdown!"
-        } else if (untilEnd === 0) {
-            msg = 'Ohi on!! https://www.youtube.com/watch?v=uWF4DXP8ZFc'
-        } sendTJ(untilEnd, msg)
+        untilEndMsg.forEach(message => {
+            if (message[untilEnd]){
+                msg=message[untilEnd] 
+            }
+        })
+        
+        sendTJ(untilEnd, msg)
     } 
     else {
         telegram.SendMessage(update.chat, 
