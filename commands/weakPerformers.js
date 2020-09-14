cc3838e43e7dfb8d3da074180292084fdc45e0b2
Lassi Timoskainen <timoskainen.lassi@gmail.com>
2020-09-14 17:18:59 +0300

    Added list of weak performers

---
+++ commands/weakPerformers.js
@@ -0,0 +1,54 @@
+const { telegram } = require('../index')
+const helpCommands = require('./help')
+const mongoUtil = require('../mongoUtil');
+
+let collection = undefined;
+let GetCollection = () => {
+    if (!collection) {
+        collection = mongoUtil.getDb().collection('heikot');
+    }
+
+    return collection;
+}
+
+exports.weakPerformers = {
+    help: 'Listaa heikot suorittajat ',
+    usage: '/heikot',
+    aliases: ['heikot', 'h', 'weakPeople', 'weak'],
+    func: (args, update) => {
+        GetCollection().find({}).toArray((err, docs) => {
+            let msg = '<b>Heikkojen suorittajien lista:</b>\n'
+            for (let doc of docs) {
+                msg += `  ${doc.name}\n`
+            }
+            telegram.SendMessage(update.chat, msg, { disable_notification: true, parse_mode: 'html' })
+        })
+    },
+}
+
+exports.addPerformer = {
+    help: 'Lisää uuden heikon suorittaja',
+    usage: '/lisääHeikko',
+    aliases: ['lisääHeikko', 'addPerformer', 'addH', 'aH'],
+    func: (args, update) => {
+        if (args.length < 1) {
+            helpCommands.usage.func([ 'addPerformer' ], update);
+            return;
+        }
+
+        let name = args.shift().trim()
+
+        GetCollection().findOne({name}, (err, result) => {
+            if (result === null ) {
+                GetCollection().insertOne({ name })
+                telegram.SendMessage(update.chat, 
+                    `Heikko suorittaja lisätty`, 
+                    { disable_notification: true })
+            } else {
+                telegram.SendMessage(update.chat, 
+                    `Kyseinen henkilö on jo suoriutunut heikosti`,
+                    { disable_notification: true })
+            }
+        })
+    }
+}
