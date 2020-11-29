eaf015765cae51e5ddf00bbd6cbb76ffcfef57bd
Lassi Timoskainen <timoskainen.lassi@gmail.com>
2020-11-29 20:39:57 +0200

    Added most of the christmas calendar photos

--- commands/christmas.js
+++ commands/christmas.js
@@ -4,37 +4,38 @@ const _ = require('lodash')
 
 const base = './images/joulukalenteri'
 const endings = [
-    '1.png', //1
-    '1.png', //2
-    '1.png', //3
-    '1.png', //4
-    '1.png', //5
-    '1.png', //6
-    '1.png', //7
-    '1.png', //8
+    'nyssetulee.png', //1
+    'tontutseuraa.png', //2
+    'leRacoon.png', //3
+    'joulusetä.png', //4
+    'pussyass.png', //5
+    'rosolli.png', //6
+    'meetöihin.png', //7
+    'booli.png', //8
     '1.png', //9
-    '1.png', //10
-    '1.png', //11
-    '1.png', //12
+    'grandtheft.png', //10
+    'viinanmäärä.png', //11
+    'sima.png', //12
     '1.png', //13
-    '1.png', //14
-    '1.png', //15
-    '1.png', //16
+    'mayhem.png', //14
+    'jokoonjoulu.png', //15
+    'ilmapallo.png', //16
     '1.png', //17
-    '1.png', //18
+    'jallu.png', //18
     '1.png', //19
     'rentoutumisluukku.jpg', //20
     '1.png', //21
     '1.png', //22
     '1.png', //23
-    'insertjouluryhmäkuva.png', //24
+    'widepukki.png', //24
+    //'insertjouluryhmäkuva.png', //25 ?
 ]
 var images = _.range(24).map((num) => `${base}/${endings[num]}`)
 
 exports.christmas = {
     help: 'Avaa päivän joulukalenteriluukun',
     usage: '/christmas',
-    aliases: ['ch'],
+    aliases: ['ch', 'luukku'],
     func: (args, update) => {
         const today = dayjs()
         const christmasDay = dayjs('2020-12-25')
