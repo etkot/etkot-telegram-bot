3d7940010f191f0c6170e5a90b8e88c1fb568e05
Lassi Timoskainen <timoskainen.lassi@gmail.com>
2020-12-05 19:38:43 +0200

    Added last christmas images

--- commands/christmas.js
+++ commands/christmas.js
@@ -4,33 +4,32 @@ const _ = require('lodash')
 
 const base = './images/joulukalenteri'
 const endings = [
-    'nyssetulee.png', //1
-    'tontutseuraa.png', //2
-    'leRacoon.png', //3
-    'joulusetä.png', //4
-    'pussyass.png', //5
-    'rosolli.png', //6
-    'meetöihin.png', //7
-    'booli.png', //8
-    'joulius.png', //9
-    'grandtheft.png', //10
-    'viinanmäärä.png', //11
-    'sima.png', //12
-    'eijoulua.png', //13
-    'mayhem.png', //14
-    'jokoonjoulu.png', //15
-    'ilmapallo.png', //16
-    '1.png', //17
-    'jallu.png', //18
-    '1.png', //19
-    'rentoutumisluukku.jpg', //20
-    '1.png', //21
-    '1.png', //22
-    '1.png', //23
-    'widepukki.png', //24
-    //'insertjouluryhmäkuva.png', //25 ?
+  'nyssetulee.png', //1
+  'tontutseuraa.png', //2
+  'leRacoon.png', //3
+  'joulusetä.png', //4
+  'pussyass.png', //5
+  'rosolli.png', //6
+  'meetöihin.png', //7
+  'booli.png', //8
+  'joulius.png', //9
+  'ilmapallo.png', //10
+  'viinanmäärä.png', //11
+  'sima.png', //12
+  'eijoulua.png', //13
+  'jokoonjoulu.png', //14
+  'credit.png', //15
+  'glog.png', //16
+  'mayhem.png', //17
+  'jallu.png', //18
+  'ryys.png', //19
+  'rentoutumisluukku.jpg', //20
+  'grandtheft.png', //21
+  'lahjaostos.png', //22
+  'widepukki.png', //23
+  'hyvääjoulua.png', //24
 ]
-var images = _.range(24).map((num) => `${base}/${endings[num]}`)
+var images = _.range(24).map(num => `${base}/${endings[num]}`)
 
 // NOTE: This will prevent the same image from being pinned multiple times
 //       but will be reset if the bot is restarted (e.g. crashes or is updated).
@@ -38,40 +37,48 @@ var images = _.range(24).map((num) => `${base}/${endings[num]}`)
 let lastDate
 
 exports.christmas = {
-    help: 'Avaa päivän joulukalenteriluukun',
-    usage: '/christmas',
-    aliases: ['ch', 'luukku'],
-    func: (args, update) => {
-        const today = dayjs()
-        const christmasDay = dayjs('2020-12-25')
+  help: 'Avaa päivän joulukalenteriluukun',
+  usage: '/christmas',
+  aliases: ['ch', 'luukku'],
+  func: (args, update) => {
+    const today = dayjs()
+    const christmasDay = dayjs('2020-12-25')
 
-        if (today.get('month') !== 11) {
-            telegram.SendMessage(update.chat, 'Ei nyt ole joulukuu!')
-        } else if (today.format() === christmasDay.format()) {
-            telegram.SendMessage(update.chat, 'Hyvää joulua!', { disable_notification: true, parse_mode: 'html' })
-        } else if (today.format() > christmasDay.format()) {
-            telegram.SendMessage(update.chat, 'Joulu meni jo :D', { disable_notification: true, parse_mode: 'html' })
-        } else {
-            let message_id = null
-            telegram.SendMessage(update.chat, 'Avataan luukkua!!').then((res) => (message_id = res.message_id))
-            telegram
-                .SendLocalPhoto(update.chat, images[today.get('date') - 1], `Luukku ${today.get('date')}`, {
-                    disable_notification: true,
-                    parse_mode: 'html',
-                })
-                .then((res) => {
-                    if (message_id) {
-                        telegram.DeleteMessage(update.chat, message_id)
-                    }
+    if (today.get('month') !== 11) {
+      telegram.SendMessage(update.chat, 'Ei nyt ole joulukuu!')
+    } else if (today.format() === christmasDay.format()) {
+      telegram.SendMessage(update.chat, 'Hyvää joulua!', {
+        disable_notification: true,
+        parse_mode: 'html',
+      })
+    } else if (today.format() > christmasDay.format()) {
+      telegram.SendMessage(update.chat, 'Joulu meni jo :D', {
+        disable_notification: true,
+        parse_mode: 'html',
+      })
+    } else {
+      let message_id = null
+      telegram
+        .SendMessage(update.chat, 'Avataan luukkua!!')
+        .then(res => (message_id = res.message_id))
+      telegram
+        .SendLocalPhoto(update.chat, images[today.get('date') - 1], `Luukku ${today.get('date')}`, {
+          disable_notification: true,
+          parse_mode: 'html',
+        })
+        .then(res => {
+          if (message_id) {
+            telegram.DeleteMessage(update.chat, message_id)
+          }
 
-                    if (lastDate !== today.date()) {
-                        telegram.PinMessage(update.chat, res.result.message_id, { disable_notification: true })
-                    }
-                    lastDate = today.date()
-                })
-                .catch((reason) => {
-                    console.log('Failed to send image')
-                })
-        }
-    },
+          if (lastDate !== today.date()) {
+            telegram.PinMessage(update.chat, res.result.message_id, { disable_notification: true })
+          }
+          lastDate = today.date()
+        })
+        .catch(reason => {
+          console.log('Failed to send image')
+        })
+    }
+  },
 }
