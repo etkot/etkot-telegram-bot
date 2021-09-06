ebc7877f5a470a57d49909b26602721afce87e64
Teemu Rautavalta <teemur11@gmail.com>
2021-09-06 20:52:28 +0300

    Fixed eslint errors

--- src/commands/tunikka.ts
+++ src/commands/tunikka.ts
@@ -9,9 +9,9 @@ export default (commander: Commander): void => {
         help: 'Lisää tunikka löydön Duon tai Wäinölöiden edessä',
 
         func: async (args, message, telegram) => {
-            const res = await axios.get<object>('http://data.itsfactory.fi/journeys/api/1/vehicle-activity')
+            const res = await axios.get<any>('http://data.itsfactory.fi/journeys/api/1/vehicle-activity')
 
-            getCollection<{ res: object }>('tunikka')
+            getCollection<{ res: any }>('tunikka')
                 .insertOne({ res: res.data })
                 .then(() => {
                     telegram.sendMessage(message.chat.id, 'Tunikka löytö lisätty', { disable_notification: true })
