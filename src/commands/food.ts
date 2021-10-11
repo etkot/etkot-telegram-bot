c16871dbf9f0958b0a21e062111e402531dee43b
Valtteri Tastula <tastula@protonmail.com>
2021-10-11 16:17:40 +0300

    Add more food commands

--- src/commands/food.ts
+++ src/commands/food.ts
@@ -329,19 +329,47 @@ export default (commander: Commander): void => {
         },
     })
 
-    commander.addCommand({
-        commands: ['fondue', 'foodpoll', 'fp'],
-        arguments: [],
-        help: 'Luo pollin ruokapaikan valitsemiselle',
+    const createFoodPoll = (
+        commandNames: string[],
+        commandHelp: string,
+        pollPrompt: string,
+        pollOptions: string[]
+    ) => {
+        commander.addCommand({
+            commands: commandNames,
+            arguments: [],
+            help: commandHelp,
 
-        func: (args, message, telegram) => {
-            telegram.sendPoll(message.chat.id, 'Fondue?', ['Reaktori', 'Newton', 'Hertsi', 'Såås'], {
-                disable_notification: true,
-                is_anonymous: false,
-                allows_multiple_answers: true,
-            })
-        },
-    })
+            func: (_args, message, telegram) => {
+                telegram.sendPoll(message.chat.id, pollPrompt, pollOptions, {
+                    disable_notification: true,
+                    is_anonymous: false,
+                    allows_multiple_answers: true,
+                })
+            },
+        })
+    };
+
+    createFoodPoll(
+        ['fondue', 'foodpoll', 'fp'],
+        'Luo pollin ruokapaikan valitsemiselle',
+        'Fondue?',
+        ['Reaktori', 'Newton', 'Hertsi', 'Såås'],
+    );
+
+    createFoodPoll(
+        ['lounashervanta', 'bh', 'baahervanta', 'lounas'],
+        'Pollaa Hervannan lounasravintolat',
+        'Bääbää nams?',
+        ['Speakeasy', 'Il Posto', 'Subway', 'Zarillo', 'Fire Wok', 'Kotipizza', 'C Sushi'],
+    );
+
+    createFoodPoll(
+        ['lounaskeskusta', 'bk', 'baakeskusta'],
+        'Pollaa keskustan lounasravintolat',
+        'Bääbää nams?',
+        ['Burger', 'Pizza', 'Sushi', 'Kiinalainen', 'Ramen', 'Salaatti', 'Siivet', 'Perussuomalainen'],
+    );
 
     commander.addCommand({
         commands: ['addfood', 'lisääruoka', 'addf', 'af'],
