9e2aa9ebcaf85199f0d8f64957018837bc4f0c60
Lassi <timoskainen.lassi@gmail.com>
2022-02-05 14:18:11 +0200

    Text-to-speech on reply

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,9 @@ import { connectToServer } from './mongoUtil'
 import { Telegram, TGEvent } from './telegram'
 import commands, { Commander } from './commands'
 
+/*
 if (process.env.DB_NAME === undefined) throw Error('No DB name in .env')
-connectToServer(process.env.DB_NAME)
+connectToServer(process.env.DB_NAME) */
 
 const telegram = new Telegram(process.env.TG_TOKEN || '')
 
